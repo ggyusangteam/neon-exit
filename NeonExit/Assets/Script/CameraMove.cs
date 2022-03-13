2e4cdad2fd3ecf14f8ddfe70fa75f7150bc53d42
ggyusang <73015717+ggyusang@users.noreply.github.com>
2022-03-13 22:30:16 +0900

    3.13수정사항
    상원이 애니메이션 적용
    모바일 애니메이션 적용 안되던 버그 수정
    벽 넘어가지는 버그 수정
    불필요한 콜라이더 삭제

--- NeonExit/Assets/Script/CameraMove.cs
+++ NeonExit/Assets/Script/CameraMove.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+
+    public float SlideSpeed;
+    public float UpSpeed;
     public GameObject player;
     Vector3 cameraPos;
+  public static bool canSlide = true;
     // Start is called before the first frame update
     void Start()
     {
+  
         cameraPos = this.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = new Vector3(player.transform.position.x + cameraPos.x, cameraPos.y, player.transform.position.z + cameraPos.z);
+    
+         this.transform.position = new Vector3(player.transform.position.x + cameraPos.x, cameraPos.y, player.transform.position.z + cameraPos.z); 
+    
+        
     }
+    /*
+    IEnumerator SlideCo(Transform transform_param)
+    {
+       float dir = -0.05f;
+        float destinationPos = transform_param.position.y + dir;
+       
+        Vector3 currentPos = transform_param.position;
+
+       
+        canSlide = false;
+        while (transform_param.position.y - destinationPos >= 0.0001f || destinationPos - transform_param.position.y >= 0.0001f)
+        {
+
+            transform_param.position = Vector3.MoveTowards(transform_param.position, new Vector3(transform.position.x, destinationPos, transform.position.z), SlideSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform_param.position = new Vector3(transform.position.x, destinationPos, transform.position.z);
+
+        while (transform_param.position.y - currentPos.y >= 0.0001f || currentPos.y - transform_param.position.y >= 0.0001f)
+        {
+
+            transform_param.position = Vector3.MoveTowards(transform_param.position, new Vector3(transform.position.x, currentPos.y, transform.position.z),UpSpeed * Time.deltaTime);
+            yield return null;
+
+        }
+        transform_param.position = new Vector3(transform.position.x, currentPos.y, transform.position.z);
+       
+        canSlide = true;
+    }
+    */
 }
