7a6d7acdee2230ff30db938c2b1b338f73c87800
ggyusang <73015717+ggyusang@users.noreply.github.com>
2022-04-18 00:51:38 +0900

    04/18 유규상 수정
    끝쪽 노란 실선 라인 추가
    턱 크기 0.5배로 수정
    길 크기 키움
    line material 0,5추가 및 linemanager
    코드 추가

--- NeonExit/Assets/LineManager.cs
+++ NeonExit/Assets/LineManager.cs
@@ -18,42 +18,49 @@ public class LineManager : MonoBehaviour
     {
         switch(Player.transform.position.x)
         {
+       
             case float n when (n<= -1.5f):
-            for(int i=1; i<lineList.Count;i++)
-            {
+                for (int i = 0; i < lineList.Count; i++)
+                {
                     lineList[i].color = Color.black;
                 }
-                lineList[0].color = new Color(0, 255, 219) * 3;
+                lineList[0].color = new Color(90, 71, 0) * 1.5f;
+                lineList[1].color = new Color(0, 255, 219) * 3;
                 break;
             case float n when (n >= -1.5f && n <-0.5f):
-                for (int i = 2; i < lineList.Count; i++)
+                for (int i = 0; i < lineList.Count; i++)
                 {
                     lineList[i].color = Color.black;
                 }
 
-                lineList[0].color = new Color(0, 255, 219) * 3;
-                lineList[1].color = new Color(0, 255, 219) * 3;
-                break;
-            case float n when (n >= -0.5f && n < 0.5f):
-            
-                lineList[0].color = Color.black;
-                lineList[3].color = Color.black;
                 lineList[1].color = new Color(0, 255, 219) * 3;
                 lineList[2].color = new Color(0, 255, 219) * 3;
+                break;
+            case float n when (n >= -0.5f && n < 0.5f):
+                for (int i = 0; i < lineList.Count; i++)
+                {
+                    lineList[i].color = Color.black;
+                }
+                lineList[2].color = new Color(0, 255, 219) * 3;
+                lineList[3].color = new Color(0, 255, 219) * 3;
                 break;
             case float n when (n >= 0.5f && n < 1.5f):
 
-                lineList[0].color = Color.black;
-                lineList[1].color = Color.black;
-                lineList[2].color = new Color(0, 255, 219) * 3;
+                for (int i = 0; i < lineList.Count; i++)
+                {
+                    lineList[i].color = Color.black;
+                }
                 lineList[3].color = new Color(0, 255, 219) * 3;
+                lineList[4].color = new Color(0, 255, 219) * 3;
                 break;
             case float n when (n >= 1.5f ):
+                for (int i = 0; i < lineList.Count; i++)
+                {
+                    lineList[i].color = Color.black;
+                }
 
-                lineList[0].color = Color.black;
-                lineList[1].color = Color.black;
-                lineList[2].color = Color.black;
-                lineList[3].color = new Color(0, 255, 219) * 3;
+                lineList[4].color = new Color(0, 255, 219) * 3;
+                lineList[5].color = new Color(90, 71, 0) * 1.5f;
                 break;
 
         }
